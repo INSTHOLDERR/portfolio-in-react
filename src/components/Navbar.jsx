3df175a3e3244cd8178c29c45fc398bcb6230ec7
Nikhil <nikhilpramod425@gmail.com>
2024-06-14 13:58:37 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
         <div>
           <ul className="nav-links">
             <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
+            <li><a href="#experience">Skills</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
